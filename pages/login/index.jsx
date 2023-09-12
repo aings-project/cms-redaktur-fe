7e58be4621823281e749944d3ebc0238e6e5b498
Yudha Haris Purnama <yudha@clade.ventures>
2023-09-12 16:37:42 +0700

    fix: import useRouter in login

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginLayout from "../../components/login/LoginLayout";
+import { useRouter } from "next/router";
 
 export default function Login() {
   const router = useRouter();
