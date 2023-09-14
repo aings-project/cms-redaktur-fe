64d5aa9756ced4c42ede5a177b7087bc336169aa
Yudha Haris Purnama <yudha@clade.ventures>
2023-09-14 21:13:07 +0700

    feat: redesign validate model

--- components/news_draft_edit/ValidateModal.jsx
+++ components/news_draft_edit/ValidateModal.jsx
@@ -5,20 +5,20 @@ export default function ValidateModal({ onClose, onValidate }) {
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-20">
-        <div className="p-4 bg-neutral-50 w-1/2 h-2/3">
+        <div className="p-4 bg-white w-1/2 h-2/3 flex flex-col rounded-md">
           <div className="flex justify-end">
-            <button onClick={onClose} className="font-semibold text-xl">
+            <button onClick={onClose} className="font-bold text-xl pb-4">
               Tutup
             </button>
           </div>
           <Editor
             placeholder="**(Opsional)** Masukkan data pendukung..."
-            className="border h-2/3 overflow-y-auto bg-white"
+            className="border overflow-y-auto bg-white flex-1"
           />
           <div className="mt-6 flex justify-center">
             <button
               onClick={onValidate}
-              className="bg-zinc-800 py-4 w-1/4 text-white font-semibold rounded-md"
+              className="bg-zinc-800 py-4 w-1/4 text-white font-semibold rounded-md hover:bg-zinc-600"
             >
               Validasi
             </button>
