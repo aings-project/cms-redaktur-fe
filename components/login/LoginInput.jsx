40e6e534c2471e67d121baf17b2082bc16e7f954
Yudha Haris Purnama <yudha@clade.ventures>
2023-10-01 16:45:26 +0700

    feat: edit login layout

--- components/login/LoginInput.jsx
+++ components/login/LoginInput.jsx
@@ -14,14 +14,14 @@ export default function LoginInput({ login }) {
       <p className="text-center text-2xl font-semibold mt-6 mb-12">
         Artificial Intelligence News Generator System
       </p>
-      <p className="text-black text-xl font-semibold mb-2">Username</p>
+      <p className="text-black text-lg font-semibold mb-1">Username</p>
       <input
         className="border border-black rounded-md w-full h-10 mb-4 px-2"
         placeholder="Masukkan username"
         value={email}
         onChange={onEmailChange}
       />
-      <p className="text-black text-xl font-semibold mb-2">Kata Sandi</p>
+      <p className="text-black text-lg font-semibold mb-1">Kata Sandi</p>
       <input
         className="border border-black rounded-md w-full h-10 px-2"
         type="password"
@@ -38,7 +38,7 @@ export default function LoginInput({ login }) {
         {!isLoading && (
           <button
             onClick={() => login({ email, password })}
-            className="w-full md:w-1/2 h-10 mt-6 bg-slate-800 text-white text-base font-semibold rounded-md"
+            className="w-full md:w-1/2 h-10 mt-6 bg-slate-800 hover:bg-slate-700 text-white text-base font-semibold rounded-md"
           >
             Login
           </button>
--- components/login/LoginLayout.jsx
+++ components/login/LoginLayout.jsx
@@ -3,9 +3,8 @@ import LoginInput from "./LoginInput";
 
 export default function LoginLayout({ login }) {
   return (
-    <div className="flex bg-slate-50 h-screen">
-      <div className="hidden md:block md:w-1/2 xl:w-3/5 bg-slate-800" />
-      <div className="px-6 flex items-center justify-center w-full md:w-1/2 xl:w-2/5">
+    <div className="flex h-[calc(100dvh)] bg-slate-800 justify-center items-center">
+      <div className="px-6 h-fit flex items-center justify-center w-full md:w-1/2 xl:w-2/5 bg-slate-100 shadow-xl py-10 rounded-md">
         <LoginInput login={login} />
       </div>
     </div>
