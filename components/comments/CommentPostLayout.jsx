c112f085aefda833cbaa48022666470272f2912c
Yudha Haris Purnama <yudha@clade.ventures>
2023-10-10 00:18:33 +0700

    feat: get draft when open comments

--- components/comments/CommentPostLayout.jsx
+++ components/comments/CommentPostLayout.jsx
@@ -2,7 +2,7 @@ import { ArrowBackOutlined } from "@mui/icons-material";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-export default function CommentPostLayout() {
+export default function CommentPostLayout({ title, content }) {
   return (
     <div className="bg-neutral-100 w-full">
       <div className="bg-white max-w-screen-lg h-full overflow-y-auto mx-auto p-6">
@@ -14,14 +14,10 @@ export default function CommentPostLayout() {
         </div>
 
         <div className="border-2 bg-white p-4 rounded-md mb-4 overflow-y-auto">
-          <p className="text-2xl font-semibold mb-2">Tsunami Dasyat Di Aceh</p>
-          <p>
-            Tsunami dahsyat melanda Aceh pada 26 Desember 2004, menewaskan lebih
-            dari 230.000 orang dan menghancurkan ribuan rumah serta
-            infrastruktur. Bencana alam ini merupakan salah satu yang terburuk
-            dalam sejarah. Upaya bantuan internasional masif segera diluncurkan
-            untuk membantu korban dan memulihkan Aceh
-          </p>
+          <p className="text-2xl font-semibold mb-2">{title}</p>
+          <ReactMarkdown className="hover:cursor-default bg-white">
+            {content}
+          </ReactMarkdown>
         </div>
       </div>
     </div>
--- components/comments/CommentsLayout.jsx
+++ components/comments/CommentsLayout.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import CommentContainerLayout from "./CommentContainerLayout";
 import CommentPostLayout from "./CommentPostLayout";
 
-export default function CommentsLayout({}) {
+export default function CommentsLayout({ newsDraft }) {
   return (
     <div className="flex bg-neutral-50 h-[calc(100dvh)] overflow-y-auto">
       <div className="flex flex-wrap lg:flex-nowrap w-full">
-        <CommentPostLayout />
+        <CommentPostLayout title={newsDraft.draft_berita.title} />
+        <CommentPostLayout title={newsDraft.draft_berita.content} />
         <CommentContainerLayout />
       </div>
     </div>
