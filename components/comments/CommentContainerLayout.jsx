513e4f857a1de63f08ff5a8e92d857542ab88702
Yudha Haris Purnama <yudha@clade.ventures>
2023-10-13 13:31:32 +0700

    fix: comments scroll behavior

--- components/comments/CommentContainerLayout.jsx
+++ components/comments/CommentContainerLayout.jsx
@@ -7,7 +7,7 @@ export default function CommentContainerLayout() {
   return (
     <div className="bg-white md:bg-slate-800 w-full md:min-w-0 md:w-3/5 lg:max-w-xl px-6 md:px-4 mx-auto flex flex-col md:h-full">
       <p className="font-semibold text-xl md:hidden">Komentar</p>
-      <div className="overflow-y-auto md:overflow-y-hidden hover:md:overflow-y-auto">
+      <div className="overflow-y-auto lg:overflow-y-hidden hover:lg:overflow-y-auto">
         <CommentItemFromOther />
         <CommentItemFromUser />
         <CommentItemFromOther />
