c726506f7b17adb85dcc1deafd5db9b8eace95ed
Yudha Haris Purnama <yudha@clade.ventures>
2024-01-05 01:29:26 +0700

    test: activities

---
+++ cypress/e2e/activity.cy.js
@@ -0,0 +1,61 @@
+/**
+ * - Activity spec
+ *   - User can navigate to Log Activity Page
+ *   - User can navigate to Detail News Draft Page from activity
+ *   - User can filter log activities
+ */
+
+describe("Commentary Spec", () => {
+  beforeEach("Login", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get('input[placeholder="Masukkan email"').type("adarmono@mail.com");
+    cy.get('input[placeholder="Masukkan Kata Sandi"').type("12345");
+    cy.get("button")
+      .contains(/^Login$/)
+      .click();
+  });
+
+  it("User can navigate to Log Activity Page", () => {
+    cy.intercept(
+      "GET",
+      "https://redaktur-backend.et.r.appspot.com/v1/activity-logs?page=1&limit=10"
+    ).as("all");
+    cy.get("button").filter(":visible").contains("Aktivitas").click();
+
+    cy.wait("@all");
+
+    cy.get("p")
+      .filter(":visible")
+      .contains("Log Aktivitas")
+      .should("be.visible");
+    cy.get(".ActivityItem").should("be.visible");
+  });
+
+  it("User can navigate to Detail News Draft Page from activity", () => {
+    cy.intercept(
+      "GET",
+      "https://redaktur-backend.et.r.appspot.com/v1/activity-logs?page=1&limit=10"
+    ).as("all");
+    cy.get("button").filter(":visible").contains("Aktivitas").click();
+
+    cy.get(".ActivityItem").should("be.visible");
+    cy.get(".ActivityItem").first().click();
+
+    cy.get("p").contains("Judul Berita");
+  });
+
+  it("User can filter log activities", () => {
+    cy.intercept(
+      "GET",
+      "https://redaktur-backend.et.r.appspot.com/v1/activity-logs?page=1&limit=10"
+    ).as("all");
+    cy.get("button").filter(":visible").contains("Aktivitas").click();
+
+    cy.get("input").filter(":visible").type("Menambahkan Draf{enter}");
+
+    cy.get(".ActivityItem").each(($el, _, __) => {
+      cy.wrap($el).contains("Menambahkan berita");
+    });
+  });
+});
